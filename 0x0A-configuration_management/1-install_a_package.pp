d74c03aff18221ae897d2053d290560ab072b626
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-02-24 08:42:21 +0100

    install

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,23 @@
+# Puppet mainfest to install Flask version 2.1.0 using pip3
+
+package { 'python3-pip':
+  ensure   => installed,
+}
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+exec { 'install_flask':
+  command  => '/usr/bin/pip3 install Flask==2.1.0',
+  path     => '/usr/local/bin:/usr/bin:/bin',
+  unless   => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require  => Package['python3-pip'],
+}
