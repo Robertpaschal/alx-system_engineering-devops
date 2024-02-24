73bb50dc6b3c1a909332ba8200bfa5efd7b0b539
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-02-24 09:45:55 +0100

    kill

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,14 +1,6 @@
 # Puppet manifest to kill a process named "killmenow" using pkill
 
-exec { 'kill_process':
-  command     => '/usr/bin/pkill killmenow',
-  path        =>['/usr/bin/bash'],
-  refreshonly => true,
-  subscribe   => Exec['check_process'],
-}
-
-exec { 'check_process':
-  command     => '/usr/bin/pgrep killmenow',
-  returns     => [0, 1],
-  refreshonly => true,
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
