3e8ac6c9d75d69347f154c60239ccdf496f6c3a3
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-02-24 08:44:43 +0100

    install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -16,8 +16,8 @@ package { 'werkzeug':
 }
 
 exec { 'install_flask':
-  command  => '/usr/bin/pip3 install Flask==2.1.0',
-  path     => '/usr/local/bin:/usr/bin:/bin',
-  unless   => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  require  => Package['python3-pip'],
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/local/bin:/usr/bin:/bin',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
 }
