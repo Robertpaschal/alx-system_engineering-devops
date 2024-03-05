bb9954c2ca4547ffdffb0b46727b89d11c6aa158
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-03-05 19:02:59 +0100

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,25 +6,25 @@ exec { 'update system':
 }
 
 package { 'nginx':
-        ensure  => 'installed',
-        require => Exec['update system']
+	ensure  => 'installed',
+	require => Exec['update system']
 }
 
 file {'/var/www/html/index.html':
-        content => 'Hello World!'
+	content => 'Hello World!'
 }
 
-exec { 'redirect_me':
-        command  => 'sed -i " 24i\       rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
-        provider => 'shell'
+exec {'redirect_me':
+	command  => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
 exec {'HTTP header':
-        command  => 'sed -i "25i\        add_header X-served-By \$hostname;" /etc/nginx/sites-available/default',
-        provider => 'shell'
+	command  => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-service { 'nginx':
-        ensure  => running,
-        require => Package['nginx']
+service {'nginx':
+	ensure  => running,
+	require => Package['nginx']
 }
